0cba6206f00bc47d0e1af935733200d485daad0b
7unho <rlawnsgh8395@naver.com>
2023-06-19 13:32:28 +0900

    chore: 프로필 이미지 변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "7unho",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "BE developer",
     bio: "'같이'를 창출하는 개발자",
     email: "rlawnsgh8395@gmail.com",
@@ -19,7 +19,7 @@ const CONFIG = {
   // blog setting (required)
   blog: {
     title: "7unho",
-    description: "개발 블로그",
+    description: "습관을 만드는 기록",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "7unho.blog",
   since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://file.notion.so/f/s/e93e9037-1c2a-4e52-95b8-49cae461db0b/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2023-04-09_20.28.58.png?id=c8a848b0-c861-486c-9339-7e8a6be58e85&table=block&spaceId=632571f2-6f4c-4829-be21-8045aa2fdeef&expirationTimestamp=1687234320086&signature=S1HRRSHgP59_AU3fppXcJl9Vga7p0t-4fLNdIfoLoxw&downloadName=%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2023-04-09+20.28.58.png", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
   },
